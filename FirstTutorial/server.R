b2678de3fdc2c201bc3e77273e2c8d161cb83714
Grigorij Schleifer <chaosambulance@googlemail.com>
2021-06-17 22:04:52 +0200

    more shiny

--- FirstTutorial/server.R
+++ FirstTutorial/server.R
@@ -1,3 +1,5 @@
 server <- function(input, output) {
-    
+    output$hist <- renderPlot({
+        hist(rnorm(100))
+    })    
 }
--- FirstTutorial/ui.R
+++ FirstTutorial/ui.R
@@ -1,9 +1,12 @@
 library(shiny)
 
 ui <- fluidPage(
+    "Cool first app, once again",
     sliderInput(inputId = "num",
                 label = "Choose a number",
                 value = 25,
                 min = 1,
-                max = 100)
+                max = 100),
+    plotOutput(outputId = "hist")
 )
+
