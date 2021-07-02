fb45398ffa07f721c5faf880f6c485ee06d890ee
Grigorij Schleifer <chaosambulance@googlemail.com>
2021-07-02 22:26:04 +0200

    some funky shiny cardiac output action

--- FirstTutorial/ui.R
+++ FirstTutorial/ui.R
@@ -1,13 +1,76 @@
 library(shiny)
 
 ui <- fluidPage(
-    "Cool first app, once again",
     sliderInput(inputId = "num",
                 label = "Choose a number",
                 value = 25,
                 min = 1,
                 max = 100),
     
+    column(6, 
+           fluidRow(column(4, numericInput("a", label = "A", 0, width = '100%')),
+                    column(4, numericInput("b", label = "B", 0, width = '100%'))),
+           fluidRow(column(4, numericInput("c", label = "C", 0, width = '100%')),
+                    column(4, numericInput("d", label = "D", 0, width = '100%')))),
+        
+    
+    
+    numericInput("Hg", "Hg", 100, min = 0, max = 100),
+    numericInput("SaO2", "SaO2", 100, min = 0, max = 100),
+    numericInput("SvO2", "SvO2", 80, min = 0, max = 100),
+    numericInput("PaO2", "PaO2", 100, min = 0, max = 100),
+    
+    
+    numericInput("PvO2", "PvO2", 100, min = 0, max = 100),
+    numericInput("SaO2", "SaO2", 100, min = 0, max = 100),
+    numericInput("SaO2", "SaO2", 100, min = 0, max = 100),
+    numericInput("SaO2", "SaO2", 100, min = 0, max = 100),
+    numericInput("SaO2", "SaO2", 100, min = 0, max = 100),
+    numericInput("SaO2", "SaO2", 100, min = 0, max = 100),
+    
     plotOutput(outputId = "hist")
 )
 
+# 
+# rm(list = ls()) 
+# 
+# Sa02 
+# 
+# blood_manual <- matrix(dimnames = list(
+#     c(
+#         
+#     )
+# ))
+# 
+# 
+# CaO2 <- as.numeric()
+# 
+# 
+# 
+# shunt_calc <- function(value) {
+#     PAO2 <- as.numeric(value["fiO2",]*(value["Patm",]-47)-(value["PaCO2",]/0.8)+2)
+#     # CAPILLARY OXYGEN CONTENT
+#     # CcO2 = (Hgb * 1.31) + (0.0031 * PAO2) 
+#     # no multiplication with the arterial sat fraction
+#     Cco2 <- as.numeric(value["Hb",]*1.34)+(0.0031*PAO2)
+#     
+#     # ARTERIAL OXYGEN CONTENT
+#     Cao2 <- as.numeric(value["Hb",]*1.34*(value["fO2art",]/100))+(0.0031*value["PaO2",])
+#     
+#     # MIXED VEONOUS OXYGEN CONTENT
+#     Cvo2 <- as.numeric(value["Hb",]*1.34*(value["fO2mven",]/100))+(0.0031*value["PvO2",])
+#     
+#     # SHUNT
+#     SHUNT <- ((Cco2-Cao2)/(Cco2-Cvo2))*100
+#     # dont forget to assign the method return to a new object
+#     # otherwise everything stays inside the method
+#     
+#     return(cat("The shunt is:", as.numeric(SHUNT), "\n", "\n",
+#                "PAO2 is", PAO2, "\n",
+#                "Cco2 is", Cco2, "\n",
+#                "Cao2 is", Cao2, "\n",
+#                "Cvo2 is", Cvo2))
+# }
+# 
+# 
+# shunt_calc(blood_manual)
