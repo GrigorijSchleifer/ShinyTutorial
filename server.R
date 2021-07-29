8bd54c87c6bda6dd18c4faaa8d3e3c64c7c03355
Grigorij Schleifer <chaosambulance@googlemail.com>
2021-07-29 22:32:44 +0200

    ggplot works for dipslaying the barplot

--- server.R
+++ server.R
@@ -1,11 +1,23 @@
 server <- function(input, output) {
     # country <- reactive({ input$country })
-    survey_data <- read.csv("/Users/grigorijschleifer/Desktop/R/Shiny/ShinyTutorial/only_scores.csv")
+    survey_data <- read.csv("/Users/grigorijschleifer/Desktop/R/Shiny/ShinyTutorial/only_scores.csv", 
+                            stringsAsFactors = TRUE, 
+                            check.names = FALSE)
     
     output$scores_barplot <- renderPlot({
-        survey_data %>% 
-            select(c("G01Q04", "G02Q26_SQ001", "G02Q26_SQ002", "G02Q26_SQ003", "G02Q26_SQ004", "G02Q26_SQ005", "G02Q26_SQ006")) %>% 
-            dplyr::filter(G01Q06 == input$country) %>% 
+        survey_data %>%
+            select(
+                c(
+                    "G01Q04",
+                    "G02Q26_SQ001",
+                    "G02Q26_SQ002",
+                    "G02Q26_SQ003",
+                    "G02Q26_SQ004",
+                    "G02Q26_SQ005",
+                    "G02Q26_SQ006"
+                )
+            ) %>%
+            dplyr::filter(G01Q06 == input$country) %>%
             ggplot(aes(x = G01Q04)) +
             geom_bar()
 })
--- try_error.R
+++ try_error.R
@@ -1,24 +1,40 @@
-new_data <- data_v %>%
-    select(c(
-        "id",
-        "G01Q06",
-        "G02Q26_SQ001",
-        "G02Q26_SQ002",
-        "G02Q26_SQ003",
-        "G02Q26_SQ004",
-        "G02Q26_SQ005",
-        "G02Q26_SQ006"
-        )
-    )
-
-colnames(new_data) <- c("id", "Country", "ASA-Class", "Apfel-Score", "NYHA-Score", "ARISCAT-Score","rRCI", "POSPOM")
-
-
-new_data <- new_data %>% 
+survey_data %>%
+    select(., -X) %>%
+    dplyr::filter(
+        Country == "Italy" &
+            !is.na(ASA.Class)) %>%
     gather(key = "Question",
            value = "Answer",
-           c(-id, -Country)) %>% 
-    mutate(Answer = as.factor(Answer)) %>% 
-    dplyr::filter(!is.na(Answer)) %>% 
-    group_by(Country) %>% 
-    summary()
+           c(-id, -Country)) %>%
+    dplyr::filter(Answer == "Yes") %>% 
+    count(Question, Answer) %>%
+    ggplot(aes(x = Question, y = n)) +
+    geom_bar(stat = "identity") 
+
+
+
+# scores_only <- survey_data %>%
+#     select(c(
+#         "id",
+#         "G01Q06",
+#         "G02Q26_SQ001",
+#         "G02Q26_SQ002",
+#         "G02Q26_SQ003",
+#         "G02Q26_SQ004",
+#         "G02Q26_SQ005",
+#         "G02Q26_SQ006"
+#         )
+#     )
+
+
+# colnames(scores_only) <-
+#     c(
+#         "id",
+#         "Country",
+#         "ASA-Class",
+#         "Apfel-Score",
+#         "NYHA-Score",
+#         "ARISCAT-Score",
+#         "rRCI",
+#         "POSPOM"
+#     )
