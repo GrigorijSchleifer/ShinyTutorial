3a352f63dc2ce82cff998bcbf7906b74b532a349
Grigorij Schleifer <chaosambulance@googlemail.com>
2021-07-31 19:44:07 +0200

    Improvins the barchart presentation that will show not filled out scores as well instead of skipping them

--- try_error.R
+++ try_error.R
@@ -1,20 +1,28 @@
-survey_data %>%
+counted_result <- survey_data %>%
     select(., -X) %>%
     dplyr::filter(
-        Country == "Germany") %>%
+        Country == "Italy") %>%
+    dplyr::filter(!is.na(ASA.Class)) %>% 
     gather(key = "Question",
            value = "Answer",
            c(-id, -Country)) %>%
-    # dplyr::filter(Answer == "Yes") %>% 
-    count(Question, Answer) %>%
-    group_by(Question) %>% 
-    ggplot(aes(x = Question, y = n)) +
+    dplyr::filter(Answer == "Yes") %>% 
+    count(Question, Answer)
+
+
+df_for_scores <- data.frame(score = c("ASA.Class", "Apfel.Score", "NYHA.Score", "ARISCAT.Score", "rRCI", "POSPOM"),
+                            n = c(1,0,0,0,0,0))
+
+for(i in counted_result$Question) {
+    df_for_scores$n[df_for_scores$score == i] <- counted_result$n[counted_result$Question == i]
+}
+
+
+df_for_scores %>% 
+    ggplot(aes(x = score, y = n)) +
     geom_col() 
 
 
-
-df_for_scores <- data.frame(Countries = c("ASA-Class", "Apfel-Score", "NYHA-Score", "ARISCAT-Score", "rRCI", "POSPOM"),
-                            n = c(1,0,0,0,0,0))
 
 
 
