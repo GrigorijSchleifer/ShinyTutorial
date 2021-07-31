f323a11b05ab1ba4fba474c3ded9e892aa694691
Grigorij Schleifer <chaosambulance@googlemail.com>
2021-07-31 02:48:48 +0200

    struggling

--- try_error.R
+++ try_error.R
@@ -1,7 +1,7 @@
 survey_data %>%
     select(., -X) %>%
     dplyr::filter(
-        Country == "Finland") %>%
+        Country == "Germany") %>%
     gather(key = "Question",
            value = "Answer",
            c(-id, -Country)) %>%
@@ -11,9 +11,23 @@ survey_data %>%
     ggplot(aes(x = Question, y = n)) +
     geom_col() 
 
-df_for_scores <- data.frame(a = c(1,2,3,4,5,6,7,8),
-           b = c("A", "B", "C", "D", "E", "F", "G", "H"))
 
+
+df_for_scores <- data.frame(Countries = c("ASA-Class", "Apfel-Score", "NYHA-Score", "ARISCAT-Score", "rRCI", "POSPOM"),
+                            n = c(1,0,0,0,0,0))
+
+
+
+
+
+
+
+# 
+# levels(df_for_scores$Countries) <-  c("ASA-Class", "Apfel-Score", "NYHA-Score", "ARISCAT-Score", "rRCI", "POSPOM")
+# 
+# df_for_scores %>% 
+#     ggplot(aes(x = Countries , y = n)) + 
+#     geom_col()
 
 
 # scores_only <- survey_data %>%
